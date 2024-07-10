c485148d0667488fabaa44153c318cdcfbcd12e5
Shanka Visal <shankavisal@gmail.com>
2024-07-11 00:30:05 +0530

    update

---
+++ src/Components/Testing.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function npm install --save react-background-slideshow() {
+  return (
+    <div>npm install --save react-background-slideshow</div>
+  )
+}
+
+export default npm install --save react-background-slideshow
